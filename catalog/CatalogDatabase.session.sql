4b43f2adc45f8dfbdce362152bd00de2682dcb7f
valkyrie <rajarshibiswas5678@gmail.com>
2025-10-06 17:31:10 +0000

    minor updated

--- catalog/CatalogDatabase.session.sql
+++ catalog/CatalogDatabase.session.sql
@@ -9,4 +9,7 @@ SELECT * FROM hotel_image;
 SELECT * FROM room;
 SELECT * FROM room_image;
 
+TRUNCATE TABLE room CASCADE;
+TRUNCATE TABLE room_image;
+
 -- DELETE FROM room WHERE room_number = 209 AND hotel_id = 'ght-ieib';
